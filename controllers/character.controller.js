14dbade4d90efcde8fe7086533bfff64d80c7dfb
Eusebio <eusgarpla@alum.us.es>
2021-04-10 18:53:40 +0200

    Created controllers for more use cases

--- controllers/character.controller.js
+++ controllers/character.controller.js
@@ -1,121 +1,140 @@
-import Character from '../models/Character';
+import Character from "../models/Character";
 
-export async function createCharacter(req, res){
-    const {
+export async function createCharacter(req, res) {
+  const { name, version, gameId } = req.body;
+
+  try {
+    let newCharacter = await Character.create({
+      name,
+      version,
+      gameId,
+    });
+
+    if (newCharacter) {
+      return res.json({
+        message: "Character created",
+        data: newCharacter,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error creating character",
+    });
+  }
+}
+
+export async function getCharacters(req, res) {
+  try {
+    let chars = await Character.findAll();
+    if (chars) {
+      return res.json({
+        data: chars,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error finding characters",
+    });
+  }
+}
+
+export async function getCharacter(req, res) {
+  const { characterId } = req.params;
+
+  try {
+    let character = await Character.findAll({
+      where: {
+        id: characterId,
+      },
+    });
+
+    if (character) {
+      return res.json({
+        data: character,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: `Error finding character with id ${characterId}`,
+    });
+  }
+}
+
+export async function deleteCharacter(req, res) {
+  const { characterId } = req.params;
+
+  try {
+    let deletedCharacter = await Character.destroy({
+      where: {
+        id: characterId,
+      },
+    });
+
+    if (deletedCharacter) {
+      return res.json({
+        message: "Character deleted",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error deleting character",
+    });
+  }
+}
+
+export async function updateCharacter(req, res) {
+  const { characterId } = req.params;
+  const { name, version } = req.body;
+
+  try {
+    let updatedCharacter = await Character.update(
+      {
         name,
         version,
-        gameId
-    } = req.body;
+      },
+      {
+        where: {
+          id: characterId,
+        },
+      }
+    );
 
-    try {
-        let newCharacter = await Character.create({
-            name,
-            version,
-            gameId
-        });
-
-        if(newCharacter)  {
-            return res.json({
-                message: 'Character created',
-                data: newCharacter
-            })
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Error creating character'
-        });
+    if (updatedCharacter) {
+      return res.json({
+        message: "Character updated",
+      });
     }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: `Error updating character ${characterId}`,
+    });
+  }
 }
 
-export async function getCharacters(req, res){
-    try {
-        let chars = await Character.findAll();
-        if(chars) {
-            return res.json({
-                data: chars
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Error finding characters'
-        });
+export async function getCharactersByGame(req, res) {
+  const { gameId } = req.params;
+
+  try {
+    let characters = await Character.findAll({
+      where: {
+        gameId: gameId,
+      },
+    });
+
+    if (characters) {
+      return res.json({
+        characters,
+      });
     }
-}
-
-export async function getCharacter(req, res){
-    const { characterId } = req.params;
-
-    try {
-        let character = await Character.findAll({
-            where: {
-                id: characterId
-            }
-        });
-
-        if(character) {
-            return res.json({
-                data: character
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: `Error finding character with id ${characterId}`
-        });
-    }
-}
-
-export async function deleteCharacter(req, res){
-    const { characterId } = req.params;
-
-    try {
-        let deletedCharacter = await Character.destroy({
-            where: {
-                id: characterId
-            }
-        });
-
-        if(deletedCharacter) {
-            return res.json({
-                message: 'Character deleted'
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Error deleting character'
-        });
-    }
-}
-
-export async function updateCharacter(req, res){
-    const { characterId } = req.params;
-    const {  
-        name,
-        version,
-    } = req.body;
-
-    try {
-        let updatedCharacter = await Character.update({
-            name,
-            version,
-        }, {
-            where: {
-                id: characterId
-            }
-        });
-
-        if(updatedCharacter) {
-            return res.json({
-                message: 'Character updated'
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        return res.status(500).json({
-            message: `Error updating character ${characterId}`
-        });
-    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: `Error getting characters of the game ${gameId}`,
+    });
+  }
 }
--- controllers/tierlist.controller.js
+++ controllers/tierlist.controller.js
@@ -1,118 +1,155 @@
-import Tierlist from '../models/Tierlist';
+import Tierlist from "../models/Tierlist";
 
-export async function createTierList(req, res){
-    const { title, likes, date, userId } = req.body;
-    try {
-        let newTierlist = await Tierlist.create({
-            title,
-            likes,
-            date,
-            userId
-        });
-        if(newTierlist){
-            return res.json({
-                message: 'Tierlist created',
-                data: newTierlist
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Error creating Tierlist'
-        });
+export async function createTierList(req, res) {
+  const { title, likes, date, userId } = req.body;
+  try {
+    let newTierlist = await Tierlist.create({
+      title,
+      likes,
+      date,
+      userId,
+    });
+    if (newTierlist) {
+      return res.json({
+        message: "Tierlist created",
+        data: newTierlist,
+      });
     }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error creating Tierlist",
+    });
+  }
 }
 
-export async function getTierlists(req, res){
-    try {
-        let tierlists = await Tierlist.findAll();
-        if(tierlists) {
-            return res.json({
-                data: tierlists
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Error finding tierlists'
-        });
+export async function getTierlists(req, res) {
+  try {
+    let tierlists = await Tierlist.findAll();
+    if (tierlists) {
+      return res.json({
+        data: tierlists,
+      });
     }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error finding tierlists",
+    });
+  }
 }
 
-export async function getTierlist(req, res){
-    const { tierlistId } = req.params;
+export async function getTierlist(req, res) {
+  const { tierlistId } = req.params;
 
-    try {
-        let tierlist = await Tierlist.findAll({
-            where: {
-                id: tierlistId
-            }
-        });
+  try {
+    let tierlist = await Tierlist.findAll({
+      where: {
+        id: tierlistId,
+      },
+    });
 
-        if(tierlist) {
-            return res.json({
-                data: tierlist
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: `Error finding tierlist ${tierlistId}`
-        });
+    if (tierlist) {
+      return res.json({
+        data: tierlist,
+      });
     }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: `Error finding tierlist ${tierlistId}`,
+    });
+  }
 }
 
-export async function deleteTierlist(req, res){
-    const {tierlistId} = req.params;
-    
-    try {
-        let deletedTierlist = await Tierlist.destroy({
-            where: {
-                id: tierlistId
-            }
-        });
+export async function deleteTierlist(req, res) {
+  const { tierlistId } = req.params;
 
-        if(deletedTierlist) {
-            return res.json({
-                message: 'Tierlist deleted'
-            });
-        }
-    } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: 'Error deleting tierlist'
-        });
+  try {
+    let deletedTierlist = await Tierlist.destroy({
+      where: {
+        id: tierlistId,
+      },
+    });
+
+    if (deletedTierlist) {
+      return res.json({
+        message: "Tierlist deleted",
+      });
     }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: "Error deleting tierlist",
+    });
+  }
 }
 
-export async function updateTierlist(req, res){
-    const { tierlistId } = req.params;
-    const {
+export async function updateTierlist(req, res) {
+  const { tierlistId } = req.params;
+  const { title, likes, date } = req.body;
+
+  try {
+    let updatedTierlist = await Tierlist.update(
+      {
         title,
         likes,
-        date
-    } = req.body;
+        date,
+      },
+      {
+        where: {
+          id: tierlistId,
+        },
+      }
+    );
+
+    if (updatedTierlist) {
+      return res.json({
+        message: "Tierlist updated",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: `Error updating tierlist ${tierlistId}`,
+    });
+  }
+}
+
+export async function getTierlistsByUserId(req, res) {
+  const { userId } = req.params;
+
+  try {
+    let tierlists = await Tierlist.findAll({
+      where: {
+        userId: userId,
+      },
+    });
+
+    if (tierlists) {
+      return res.json({ tierlists });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: `Error getting tierlists from user ${userId}`,
+    });
+  }
+}
+
+export async function getTierlistsByGameId(req, res){
+    const { gameId } = req.params;
 
     try {
-        let updatedTierlist = await Tierlist.update({
-            title,
-            likes,
-            date
-        }, {
+        let tierlists = await Tierlist.findAll({
             where: {
-                id: tierlistId
+                gameId: gameId
             }
         });
-
-        if(updatedTierlist) {
-            return res.json({
-                message: 'Tierlist updated'
-            });
-        }
     } catch(e) {
-        console.log(e);
-        res.status(500).json({
-            message: `Error updating tierlist ${tierlistId}`
-        });
+        console.log(e)
+        return res.status(500).json({
+            message: `Error getting tierlists from game ${gameId}`,
+        })
     }
 }
